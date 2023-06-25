0d13b04bf9390c1400e52e6154061a5876f8345c
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-25 15:39:34 +0330

    post request completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "nutritionix.com"
+question = input("Tell me which exersice you did: ")
 
 headers = {
     "x-app-id": "a41d7881",
@@ -10,16 +10,20 @@ headers = {
 }
 
 params = {
-    "query": "ran 3 miles",
+    "query": question,
     "gender": "female",
     "weight_kg": 72.5,
     "height_cm": 167.64,
     "age": 30
 }
 
-NLP_endpoint = "/v2/search/instant"
+url = "https://trackapi.nutritionix.com/v2"
+NLP_endpoint = "/natural/exercise"
 
-response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise",
+response = requests.post(url=f"{url}{NLP_endpoint}",
                          json=params, headers=headers)
+
+with open("data.json", mode="w") as file:
+    file.write(response.text)
 
 print(response.text)
