c708c456a06445b996a473e3ed382cc331735b6e
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-25 21:27:25 +0330

    project finished

--- main.py
+++ main.py
@@ -1,73 +1,29 @@
 import requests
-from datetime import datetime
-import os
 
-GENDER = YOUR GENDER
-WEIGHT_KG = YOUR WEIGHT
-HEIGHT_CM = YOUR HEIGHT
-AGE = YOUR AGE
-
-APP_ID = os.environ["YOUR_APP_ID"]
-API_KEY = os.environ["YOUR_API_KEY"]
-
-exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheet_endpoint = os.environ["YOUR_SHEET_ENDPOINT"]
-
-exercise_text = input("Tell me which exercises you did: ")
+question = input("Tell me which exersice you did: ")
 
 headers = {
-    "x-app-id": APP_ID,
-    "x-app-key": API_KEY,
+    "x-app-id": "a41d7881",
+    "x-app-key": "0293ec30da232b1567f437a2a618f009",
+    "x-remote-user-id": "0",
+    "Content-Type": "application/json",
 }
 
-parameters = {
-    "query": exercise_text,
-    "gender": GENDER,
-    "weight_kg": WEIGHT_KG,
-    "height_cm": HEIGHT_CM,
-    "age": AGE
+params = {
+    "query": question,
+    "gender": "female",
+    "weight_kg": 72.5,
+    "height_cm": 167.64,
+    "age": 30
 }
 
-response = requests.post(exercise_endpoint, json=parameters, headers=headers)
-result = response.json()
-print(result)
+url = "https://trackapi.nutritionix.com/v2"
+NLP_endpoint = "/natural/exercise"
 
-today_date = datetime.now().strftime("%d/%m/%Y")
-now_time = datetime.now().strftime("%X")
+response = requests.post(url=f"{url}{NLP_endpoint}",
+                         json=params, headers=headers)
 
-for exercise in result["exercises"]:
-    sheet_inputs = {
-        "workout": {
-            "date": today_date,
-            "time": now_time,
-            "exercise": exercise["name"].title(),
-            "duration": exercise["duration_min"],
-            "calories": exercise["nf_calories"]
-        }
-    }
+with open("data.json", mode="w") as file:
+    file.write(response.text)
 
-    #No Auth
-    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
-
-
-    #Basic Auth
-    sheet_response = requests.post(
-        sheet_endpoint,
-        json=sheet_inputs,
-        auth=(
-            os.environ["USERNAME"],
-            os.environ["PASSWORD"],
-        )
-    )
-
-    #Bearer Token
-    bearer_headers = {
-    "Authorization": f"Bearer {os.environ['TOKEN']}"
-    }
-    sheet_response = requests.post(
-        sheet_endpoint,
-        json=sheet_inputs,
-        headers=bearer_headers
-    )
-
-    print(sheet_response.text)
+print(response.text)
